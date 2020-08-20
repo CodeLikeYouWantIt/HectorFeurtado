7cdb729c5e7334dd6a4a3620a335c1cac5ca6ee9
Hec <HectorFeurtado@gmail.com>
2020-08-19 21:05:29 -0400

    set up react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+    constructor(){
+      super();
+  
+    }
+  
+    render (){
+      return (
+        <div className="App">
+        <h1>Hello React!!!</h1>
+        </div>
+      )
+    }
+  }
+  
+  export default App;
